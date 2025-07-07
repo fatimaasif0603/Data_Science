0ab59543daca698ba9b3e16d0354b6dd6e8cf3a0
fatimaasif0603 <fatimaasif0603@gmail.com>
2025-07-07 19:48:03 +0500

    update project01

--- project_01.py
+++ project_01.py
@@ -16,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train, y_train)
 
-# Predict and check accuracy
+# Predict and check the accuracy
 predictions = model.predict(X_test)
 print(f"Accuracy: {accuracy_score(y_test, predictions) * 100:.2f}%")
 
